40f8311e862a96ec06008128ef9b31ff56692b43
Poklonsky Alex <Aleksey.Poklonsky@infotecs.ru>
2025-06-24 07:35:24 +0300

    adjust difficulty

--- difficulty_multiplyer.py
+++ difficulty_multiplyer.py
@@ -1,3 +1,7 @@
 def get_difficulty(game_rounds: int) -> int:
     print("Request to get difficulty for round " + str(game_rounds))
+    if (game_rounds > 5):
+        return 3
+    elif (game_rounds > 10):
+        return 4
     return 2
